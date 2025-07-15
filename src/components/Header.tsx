a698548008cf6ad1fe721a40562ee0d4635ee3af
Roman Zelenin <roman_zelenin@hotmail.com>
2025-07-15 12:37:39 +0300

    fix layout for mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,15 @@
-import { Stack, Text, HStack } from '@chakra-ui/react';
-import { Time } from './Time';
-import { ThemeSwitcher } from './ThemeSwitcher';
-
-
+import { HStack, Stack, Text } from "@chakra-ui/react";
+import { ThemeSwitcher } from "./ThemeSwitcher";
+import { Time } from "./Time";
 
 export const Header = () => {
-  return (<Stack direction={'row'} justify={'center'}>
-    <Text fontSize={'28px'}>Погода на неделю</Text>
-    <HStack position={'absolute'} right={'16px'}>
-      <Time />
-      <ThemeSwitcher />
-    </HStack>
-  </Stack>);
+  return (
+    <Stack direction={{ base: "column", md: "row" }} justify={"center"} align={'center'}>
+      <Text fontSize={"28px"}>Погода на неделю</Text>
+      <HStack position={{ base: "inherit", md: "absolute" }} right={"16px"}>
+        <Time />
+        <ThemeSwitcher />
+      </HStack>
+    </Stack>
+  );
 };
